faf54cc6b38a8d2d1bbb250b498730c782a99866
Adrian Ciolek <ciolek.adrian@protonmail.com>
2024-04-23 13:17:54 +0200

    Small fix

--- bot/i18n.py
+++ bot/i18n.py
@@ -15,5 +15,5 @@ for locale in locales:
     with Path.open(Path("bot", "locales", f"{locale}.json")) as f:
         locales_data[locale] = json.load(f)
 i18n = I18n(
-    bot, consider_user_locale=False, en_US=locales_data["en"], pl=locales_data["pl"]
+    bot, consider_user_locale=True, en_US=locales_data["en"], pl=locales_data["pl"]
 )
