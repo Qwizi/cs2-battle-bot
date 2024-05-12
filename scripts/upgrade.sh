0cecc4a71737217e29ed160078448633f68703f4
Adrian Ciolek <ciolek.adrian@protonmail.com>
2024-05-12 18:23:15 +0200

    Updated upgrade script

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -25,14 +25,14 @@ docker network create --attachable cs2-battle-bot-network 2>/dev/null
 docker compose --env-file cs2-battle-bot/.env -f cs2-battle-bot/docker-compose.yml up -d --pull always --remove-orphans --force-recreate
 
 # Run app migrations
-docker compose exec app python manage.py migrate
+docker compose exec --env-file cs2-battle-bot/.env -f cs2-battle-bot/docker-compose.yml app python manage.py migrate
 
 # Check if there are any superusers in the database
-SUPERUSER_COUNT=$(docker compose exec app python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(is_superuser=True).count());")
+SUPERUSER_COUNT=$(docker compose --env-file cs2-battle-bot/.env -f cs2-battle-bot/docker-compose.yml exec app python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(is_superuser=True).count());")
 
 # Create superuser if there are no superusers in the database
-if [ "$SUPERUSER_COUNT" -eq 0 ]; then
-    docker compose exec app python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME"
+if [ "$SUPERUSER_COUNT" = "0" ]; then
+    docker compose --env-file cs2-battle-bot/.env -f cs2-battle-bot/docker-compose.yml exec app python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME"
     echo "Superuser $DJANGO_SUPERUSER_USERNAME created with password $DJANGO_SUPERUSER_PASSWORD"
     echo "Please change the password after the first login."
 
@@ -41,11 +41,11 @@ if [ "$SUPERUSER_COUNT" -eq 0 ]; then
 fi
 
 # Check if there any api keys in the database
-API_KEY_COUNT=$(docker compose exec app python manage.py shell -c "from rest_framework_api_key.models import APIKey; print(APIKey.objects.all().count());")
+API_KEY_COUNT=$(docker compose --env-file cs2-battle-bot/.env -f cs2-battle-bot/docker-compose.yml exec app python manage.py shell -c "from rest_framework_api_key.models import APIKey; print(APIKey.objects.all().count());")
 
 # Create API key if there are no api keys in the database
-if [ "$API_KEY_COUNT" -eq 0 ]; then
-    API_KEY=$(docker compose exec app python manage.py shell -c "from rest_framework_api_key.models import APIKey; api_key, key = APIKey.objects.create_key(name='cs2-battle-bot'); print(key);")
+if [ "$API_KEY_COUNT" = "0" ]; then
+    API_KEY=$(docker compose --env-file cs2-battle-bot/.env -f cs2-battle-bot/docker-compose.yml exec app python manage.py shell -c "from rest_framework_api_key.models import APIKey; api_key, key = APIKey.objects.create_key(name='cs2-battle-bot'); print(key);")
     echo "Api key created: $API_KEY."
 
     # set API_KEY to env file
@@ -55,13 +55,13 @@ if [ "$API_KEY_COUNT" -eq 0 ]; then
 fi
 
 # Check map count in database
-MAP_COUNT=$(python manage.py shell -c "from matches.models import Map; print(Map.objects.all().count());")
+MAP_COUNT=$(docker compose --env-file cs2-battle-bot/.env -f cs2-battle-bot/docker-compose.yml exec app python manage.py shell -c "from matches.models import Map; print(Map.objects.all().count());")
 
 # Load map fixtures if there are no maps in the database
-if [ "$MAP_COUNT" -eq 0 ]; then
-    docker compose exec app python manage.py loaddata maps
+if [ "$MAP_COUNT" = "0" ]; then
+    docker compose --env-file cs2-battle-bot/.env -f cs2-battle-bot/docker-compose.yml exec app python manage.py loaddata maps
 fi
 
-docker compose exec app python manage.py loaddata maps
+docker compose --env-file cs2-battle-bot/.env -f cs2-battle-bot/docker-compose.yml exec app python manage.py loaddata maps
 echo "CS2 Battle BOT has been updated to the latest version!"
 echo "Please check the logs to make sure everything is running fine."
